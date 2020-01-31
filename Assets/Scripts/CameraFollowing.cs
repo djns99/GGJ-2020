c06f6c26525f8adb2116fbb0167b1b8f151ebdfb
Shawn Yang <ases60517@gmail.com>
2020-02-01 09:31:22 +1300

    Add camera following script

--- Assets/Scripts/CameraFollowing.cs
+++ Assets/Scripts/CameraFollowing.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class CameraFollowing : MonoBehaviour
 {
     private Transform followingObject;
+    private Vector3 prevCarPos;
+    private Vector3 currCarPos;
     // Start is called before the first frame update
     void Start()
     {
         followingObject = GameObject.FindGameObjectWithTag("Player").transform;
+        prevCarPos = followingObject.transform.localPosition;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 playerPosition = followingObject.position;
-
-       // transform.position = playerPosition;
+        currCarPos = followingObject.transform.localPosition;
+        Vector3 newCamPos = new Vector3(prevCarPos.x - currCarPos.x, 0, 0);
+        Debug.Log(newCamPos);
+        transform.position -= newCamPos;
+        prevCarPos = followingObject.transform.localPosition;
     }
 }
