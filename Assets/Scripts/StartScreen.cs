7b5f32dab52f6cd5c4f0e11f1453abca1f8b6075
manuvimalmohan <manuvimalm@gmail.com>
2020-02-01 19:07:43 +1300

    username page

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour
 {
+    public void UserName()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
