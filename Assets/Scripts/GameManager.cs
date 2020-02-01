b85cd4b73a40aa4f62a21dc3c173f5058114aaf0
Shawn Yang <ases60517@gmail.com>
2020-02-01 22:11:33 +1300

    Added GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public string player_name;
+    public int score;
+
+    public static GameManager Instance { get => _instance; set => _instance = value; }
+
+    void Awake()
+    {
+        if (_instance != null)
+            Destroy(_instance);
+        else
+            _instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public void AssignPlayer()
+    {
+        //player_name = 
+        GameObject inputField = GameObject.Find("InputField");
+        if ( inputField != null)
+        {
+            player_name = inputField.transform.Find("Text").GetComponent<Text>().text;
+        }
+        if (player_name == "") player_name = "Player1";
+    }
+
+    public void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "CarScene")
+        {
+            if (GameObject.Find("Car") != null)
+                score += 1;
+            else
+            {
+                Debug.Log(score);
+            }
+        }
+    }
+}
