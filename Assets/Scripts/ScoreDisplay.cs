abafef5ef3d247308e19a61b0868d6ad4fa70a77
Shawn Yang <ases60517@gmail.com>
2020-02-02 12:17:39 +1300

    scoring

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance != null)
+        {
+            Debug.Log(GetComponent<TextMeshProUGUI>());
+            Debug.Log(GameManager.Instance.score.ToString());
+            GetComponent<TextMeshProUGUI>().text = GameManager.Instance.score.ToString();
+        }
+    }
+}
