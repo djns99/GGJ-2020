da99752af7c804a4121ae539d6334d01d8f72a13
Cameron Simmonds <cameronsimmondspersonal@gmail.com>
2020-02-02 13:16:29 +1300

    Make line a little bigger

--- Assets/Scripts/LineDraw.cs
+++ Assets/Scripts/LineDraw.cs
@@ -50,7 +50,7 @@ public class LineDraw : MonoBehaviour
         if ( Input.GetMouseButton( 0 ) )
         {
             var finger_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            if( Vector2.Distance( finger_pos, line_points[line_points.Count - 1 ]) > 1f)
+            if( Vector2.Distance( finger_pos, line_points[line_points.Count - 1 ]) > 2f)
             {
                 line_points.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
                 line_renderer.positionCount++;
@@ -61,15 +61,15 @@ public class LineDraw : MonoBehaviour
         {
             List<Vector2> edgePoints = new List<Vector2>();
             float halfWidth = line_renderer.startWidth / 2f;
-            line_renderer.Simplify(0.1f);
+            line_renderer.Simplify(0.5f);
 
             for (int i = 1; i < line_points.Count; i++)
             {
                 Vector2 distanceBetweenPoints = line_points[i - 1] - line_points[i];
                 Vector3 crossProduct = Vector3.Cross(distanceBetweenPoints, Vector3.forward);
 
-                int num_smoved = 4;
-                float reduce = i - 1 < num_smoved ? -halfWidth + (line_renderer.startWidth / num_smoved) * (i - 1) : halfWidth; 
+                int num_smoved = 5;
+                float reduce = i - 1 < num_smoved ? -halfWidth + (halfWidth / num_smoved) * (i - 1) : 0; 
 
                 Vector2 up = (reduce) * new Vector2(crossProduct.normalized.x, crossProduct.normalized.y) + line_points[i - 1];
                 Vector2 down = -(halfWidth) * new Vector2(crossProduct.normalized.x, crossProduct.normalized.y) + line_points[i - 1];
@@ -79,7 +79,7 @@ public class LineDraw : MonoBehaviour
 
                 if (i == line_points.Count - 1)
                 {
-                    up = halfWidth * new Vector2(crossProduct.normalized.x, crossProduct.normalized.y) + line_points[ i ];
+                    up = line_points[ i ];
                     down = -halfWidth * new Vector2(crossProduct.normalized.x, crossProduct.normalized.y) + line_points[ i ];
                     edgePoints.Insert(0, down);
                     edgePoints.Add(up);
